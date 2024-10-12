12237d3132fecf6847044c868b5a7126c66ede37
KopperIfr <kopper.private@gmail.com>
2024-10-12 14:03:32 +0400

    .
    .

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,16 @@
 const { ethers } = require('hardhat');
+const helper = require('@nomicfoundation/hardhat-toolbox/src/network-helpers');
 
 const main = async () => {
+
+    console.log(helper);
     
     // Deploying MarketPlace Contract..
     const MarketPlaceFactory = await ethers.getContractFactory('MarketPlace');
     const MarketPlace = await MarketPlaceFactory.deploy(23987,);
     await MarketPlace.waitForDeployment();
 
-    return {MarketPlace}
+    return MarketPlace.target;
 }
 
 main()
