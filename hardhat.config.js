fdc86fed27ed241833e4eb1cac251baca843ccc6
KopperIfr <kopper.private@gmail.com>
2024-10-12 10:31:06 +0400

    .
    .

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {version: "0.8.27"}
+    ]
+  },
+};
