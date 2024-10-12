949c78d77fc957c9e7818a88a22ff316acdf3484
KopperIfr <kopper.private@gmail.com>
2024-10-12 13:56:18 +0400

    .
    .

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const { ethers } = require('hardhat');
 
 const main = async () => {
+    
     // Deploying MarketPlace Contract..
     const MarketPlaceFactory = await ethers.getContractFactory('MarketPlace');
     const MarketPlace = await MarketPlaceFactory.deploy(23987,);
